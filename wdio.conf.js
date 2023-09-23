39243c5a9748cacb26e3162532c64d5c1d32d065
amandastecz <amandasstecz@gmail.com>
2023-09-22 23:11:47 -0300

    chore():test

--- wdio.conf.js
+++ wdio.conf.js
@@ -26,7 +26,7 @@ exports.config = {
     //
     specs: [
         // ToDo: define location for spec files here
-        '.test/specs/**/*.js'
+        '.test/specs/*.js'
     ],
     // Patterns to exclude.
     exclude: [
