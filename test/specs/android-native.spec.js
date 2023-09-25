8bb1bcf78c994cd84714fa30c427e906f04d4e6c
amandastecz <amandasstecz@gmail.com>
2023-09-25 20:46:48 -0300

    test(android): Horizontal scrolling

--- test/specs/android-native.spec.js
+++ test/specs/android-native.spec.js
@@ -17,7 +17,7 @@ describe('Android native feature tests', ()=>{
         await expect($('//*[@resource-id="android:id/alertTitle"]')).not.toExist();
     });
 
-    it.only('Vertical scrolling', async ()=>{
+    it('Vertical scrolling', async ()=>{
         await $('~App').click();
         await $('~Activity').click();
         // scroll to end (not stable if element gets moved)
@@ -26,5 +26,13 @@ describe('Android native feature tests', ()=>{
         await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollTextIntoView("Secure Surfaces")').click();
         // await $('~Secure Surfaces').click();
         await expect($('~Secure Dialog').toExist());
+    });
+
+    it.only('Horizontal scrolling', async ()=>{
+        await driver.startActivity("io.appium.android.apis", "io.appium.android.apis.view.Gallery1");
+        // horizontal scrolling
+        await $('android=new UiScrollable(new UiSelector().scrollable(true)).setAsHorizontalList().scrollForward()');
+        await $('android=new UiScrollable(new UiSelector().scrollable(true)).setAsHorizontalList().scrollBackward()');
+        //await expect($('~Secure Dialog').toExist());
     });
 });
