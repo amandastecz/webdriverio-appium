2da6e3360abdbcfb71bb7d3739b7c8f4f3768f52
amandastecz <amandasstecz@gmail.com>
2023-09-25 21:45:49 -0300

    test(android): Scrolling Exercise

--- test/specs/android-native.spec.js
+++ test/specs/android-native.spec.js
@@ -28,11 +28,29 @@ describe('Android native feature tests', ()=>{
         await expect($('~Secure Dialog').toExist());
     });
 
-    it.only('Horizontal scrolling', async ()=>{
+    it('Horizontal scrolling', async ()=>{
         await driver.startActivity("io.appium.android.apis", "io.appium.android.apis.view.Gallery1");
         // horizontal scrolling
         await $('android=new UiScrollable(new UiSelector().scrollable(true)).setAsHorizontalList().scrollForward()');
         await $('android=new UiScrollable(new UiSelector().scrollable(true)).setAsHorizontalList().scrollBackward()');
         //await expect($('~Secure Dialog').toExist());
+    });
+
+    it.only('Scrolling Exercise', async ()=>{
+        // View -> Date widgets -> Dialog
+        await driver.startActivity("io.appium.android.apis", "io.appium.android.apis.view.DateWidgets1");
+        // Get the current date
+        const currentDate = await $('//*[@resource-id="io.appium.android.apis:id/dateDisplay"]').getText();
+        // Click change date button
+        await $('~change the date').click();
+        // Scroll horizontally to the righ
+        await $('android=new UiScrollable(new UiSelector().scrollable(true)).setAsHorizontalList().scrollForward()');
+        // Pick the 10th date from that month
+        await $('//*[@text="10"]').click();
+        // Click OK button
+        await driver.acceptAlert();
+        // Assert the date is updated
+        const newDate = await $('//*[@resource-id="io.appium.android.apis:id/dateDisplay"]').getText();
+        expect(currentDate).not.toBe(newDate);
     });
 });
